c99d225b9cbf74da6e5c9d4fc4a2b2359bcd2ddd
qiqisan <qiqisan1@126.com>
2017-04-10 11:39:47 +0800

    fix

--- src/PickMethod.php
+++ src/PickMethod.php
@@ -4,12 +4,34 @@ namespace FSth\Picker;
 
 class PickMethod
 {
-    public static function random($result)
+    public static function random($result, $nums = 1)
     {
         if (empty($result) || !is_array($result)) {
             return [];
         }
-        $rand = rand(0, count($result) - 1);
-        return $result[$rand];
+        $randomResult = self::randomMethod($result, $nums);
+        return $nums == 1 ? $randomResult[0] : $randomResult;
+    }
+
+    private static function randomMethod($result, $times = 1)
+    {
+        $randKeys = [];
+        $randResult = [];
+        $randTimes = $times;
+        $resultCount = count($result);
+        if ($resultCount < $times) {
+            return $result;
+        }
+        do {
+            $randNum = rand(0, 99999);
+            $randKey = $randNum % $resultCount;
+            if (is_array($randKey)) {
+                continue;
+            }
+            $randKeys[] = $randKey;
+            $randResult[] = $result[$randKey];
+            $randTimes--;
+        } while ($randTimes > 0);
+        return $randResult;
     }
 }
